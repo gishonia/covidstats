e61c3e7982726783461e9e6694fd98d5376384cc
Giorgi Shonia <gishonia@gmail.com>
2020-04-16 22:14:46 +0400

    v1

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,12 @@ fetch("https://corona-api.com/countries/ge")
 .then(response => response.json())
 .then(data => {
    
-    var active = parseInt(data.data.latest_data.confirmed) - parseInt(data.data.latest_data.recovered) - parseInt(data.data.latest_data.deaths);
-//     $('#covid').append(`Covid-19 მიმდინარე სტატისტიკა საქართველოში:
-//     სულ დაინფიცირებული: ${data.data.latest_data.confirmed}, 
-//     გამოჯანმრთელებული: ${data.data.latest_data.recovered},
-//     დაღუპული: ${data.data.latest_data.deaths},
-//     ამჟამად დაინფიცირებული: ${active}`);
+    let active = data.data.latest_data.confirmed - data.data.latest_data.recovered - data.data.latest_data.deaths ;
+    $('#todayStats').append(`Covid-19 მიმდინარე სტატისტიკა საქართველოში:
+    სულ დაინფიცირებული: ${data.data.latest_data.confirmed}, 
+    გამოჯანმრთელებული: ${data.data.latest_data.recovered},
+    დაღუპული: ${data.data.latest_data.deaths},
+    ამჟამად დაინფიცირებული: ${active}`);
+$('#updated_at').append(`მონაცემები ბოლოს განახლდა - ${data.data.updated_at}`)
 console.log(data)
 })
